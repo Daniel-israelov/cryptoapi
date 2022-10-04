3efb06b09c07fa99f9869d913e70580eae5598ae
Daniel Israelov <svhrkz@gmail.com>
2022-10-04 11:56:02 +0300

    Initial commit

---
+++ src/main/java/com/example/cryptoapi/CryptoApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.cryptoapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CryptoApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CryptoApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/cryptoapi/CryptoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cryptoapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CryptoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
