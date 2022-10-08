6286c72d38a3aab7519f9e5ce07661d60eae5c1d
idan-montekyo <idanmontekyo@gmail.com>
2022-10-08 16:48:41 +0300

    Updated Database initialization, and added a task executor to the application.

--- src/main/java/com/example/cryptoapi/CryptoApiApplication.java
+++ src/main/java/com/example/cryptoapi/CryptoApiApplication.java
@@ -2,7 +2,11 @@ package com.example.cryptoapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+import java.util.concurrent.Executor;
 
 @SpringBootApplication
 @EnableAsync
@@ -12,4 +16,16 @@ public class CryptoApiApplication {
         SpringApplication.run(CryptoApiApplication.class, args);
     }
 
+    @Bean
+    public Executor taskExecutor() {
+        ThreadPoolTaskExecutor taskExecutor = new ThreadPoolTaskExecutor();
+        taskExecutor.setCorePoolSize(3);
+        taskExecutor.setMaxPoolSize(5);
+        taskExecutor.setKeepAliveSeconds(1);
+        taskExecutor.setQueueCapacity(100);
+
+        taskExecutor.setThreadNamePrefix("TaskExecutor");
+        taskExecutor.initialize();
+        return taskExecutor;
+    }
 }
--- src/main/java/com/example/cryptoapi/initdb/SeedDB.java
+++ src/main/java/com/example/cryptoapi/initdb/SeedDB.java
@@ -4,53 +4,131 @@ import com.example.cryptoapi.entities.Coin;
 import com.example.cryptoapi.entities.CoinType;
 import com.example.cryptoapi.entities.User;
 import com.example.cryptoapi.entities.Wallet;
+import com.example.cryptoapi.repositories.CoinRepository;
+import com.example.cryptoapi.repositories.CoinTypeRepository;
+import com.example.cryptoapi.repositories.UserRepository;
+import com.example.cryptoapi.repositories.WalletRepository;
+import com.example.cryptoapi.services.ExternalEndpointService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.CommandLineRunner;
-import org.springframework.context.annotation.Bean;
-import org.springframework.stereotype.Component;
+import org.springframework.context.annotation.*;
 
-import java.util.Collections;
 import java.util.List;
+import java.util.concurrent.CompletableFuture;
 
 /**
- * This class is responsible for the initial Databate initialization.
+ * This class is responsible for Database initialization.
  */
-@Component
+@Configuration
 @Slf4j
 public class SeedDB {
 
+    /**
+     * This method calls the {@link org.springframework.scheduling.annotation.Async} method
+     * in the {@link ExternalEndpointService} to initialize all {@link CoinType} objects
+     * from an external endpoint.
+     */
     @Bean
-    CommandLineRunner seed() {
+    public CommandLineRunner initCoinTypes(ExternalEndpointService externalEndpointService,
+                                           CoinTypeRepository coinTypeRepository) {
         return args -> {
+            log.info("\n\t\t\tinit CoinTypes . . .");
+            CompletableFuture<List<CoinType>> completableFutureCoinTypes = externalEndpointService.pullExternalData();
+            completableFutureCoinTypes.join();
+            List<CoinType> coinTypeList = completableFutureCoinTypes.get();
+            int iterationNumber = 1;
+            for (CoinType coinType : coinTypeList) {
+                log.info("Saved to repo CoinType {}/{} = {}",
+                        iterationNumber++, coinTypeList.size(), coinTypeRepository.save(coinType));
+            }
+        };
+    }
 
-            log.info("init first user");
-            User idan = new User(12345L, "idan", "montekyo", true, 25);
-            log.info("idan = " + idan);
-            log.info("idan's wallet = " + idan.getWallet());
+    @Bean
+    CommandLineRunner initUsersAndWallets(UserRepository userRepository, WalletRepository walletRepository) {
+        return args -> {
+            log.info("\n\t\t\tinit Users . . .");
+            log.info("idan = " + userRepository.save(
+                    new User(123L, "idan", "montekyo", true, 22)));
+            log.info("daniel = " + userRepository.save(
+                    new User(1L, "daniel", "israelov", true, 33)));
 
-            log.info("creating a bitcoin");
-            CoinType bitcoin = new CoinType("Bitcoin", "https://...", 25000.0, 48390432980.0,
-                    25670.0, 18992.0, 855.7);
-            log.info("bitcoin = " + bitcoin);
+            log.info("\n\t\t\tinit Wallets . . .");
+            log.info("coinbase = " + walletRepository.save(new Wallet("Coinbase")));
+            log.info("trezor = " + walletRepository.save(new Wallet("Trezor")));
+            log.info("ledger = " + walletRepository.save(new Wallet("Ledger")));
+            log.info("exodus = " + walletRepository.save(new Wallet("Exodus")));
+            log.info("mycelium = " + walletRepository.save(new Wallet("Mycelium")));
+        };
+    }
 
-            log.info("creating concrete bitcoin");
+    @Bean
+    CommandLineRunner connectBetweenUsersAndWallets(UserRepository userRepository, WalletRepository walletRepository) {
+        return args -> {
+            log.info("\n\t\t\tconnect between users and wallets . . .");
+            User idan = userRepository.findByIdentityNumber(123L);
+            Wallet coinbase = walletRepository.findByProvider("Coinbase");
+            Wallet exodus = walletRepository.findByProvider("Exodus");
+            idan.connectWallets(coinbase, exodus);
+            log.info("connecting 'coinbase' and 'exodus' wallets to idan = " + idan.connectWallets(coinbase, exodus));
+            log.info("saved 'coinbase' with idan as owner = " + walletRepository.save(coinbase));
+            log.info("saved 'exodus' with idan as owner = " + walletRepository.save(exodus));
+            userRepository.save(idan);
+            log.info("saved idan with new wallets in repo = " + idan);
+
+            User daniel = userRepository.findByIdentityNumber(1L);
+            Wallet ledger = walletRepository.findByProvider("Ledger");
+            log.info("connecting 'coinbase' and 'ledger' wallets to daniel = " + daniel.connectWallets(coinbase, ledger));
+            log.info("saved 'coinbase' with daniel as owner = " + walletRepository.save(coinbase));
+            log.info("saved 'ledger' with daniel as owner = " + walletRepository.save(ledger));
+            userRepository.save(daniel);
+            log.info("saved daniel in repo = " + daniel);
+        };
+    }
+
+    @Bean
+    CommandLineRunner initAndDistributeConcreteCryptoCoins(CoinTypeRepository coinTypeRepository,
+                                                           CoinRepository coinRepository,
+                                                           UserRepository userRepository,
+                                                           WalletRepository walletRepository) {
+        return args -> {
+            log.info("\n\t\t\tinit concrete Coins . . .");
+            CoinType bitcoin = coinTypeRepository.findByName("Bitcoin");
+            CoinType ethereum = coinTypeRepository.findByName("Ethereum");
+            CoinType cardano = coinTypeRepository.findByName("Cardano");
+            CoinType polkadot = coinTypeRepository.findByName("Polkadot");
+
             Coin concreteBitcoin = new Coin(bitcoin);
-            log.info("concrete bitcoin = " + concreteBitcoin);
-
-            log.info("creating an ethereum");
-            CoinType ethereum = new CoinType("Ethereum", "https://...", 11000.0, 5682980.0,
-                    11270.0, 10892.0, 155.7);
-            log.info("ethereum = " + bitcoin);
-
-            log.info("creating concrete ethereum");
+            log.info("Creating concrete Bitcoin = " + coinRepository.save(concreteBitcoin));
             Coin concreteEthereum = new Coin(ethereum);
-            log.info("concrete ethereum = " + concreteEthereum);
+            log.info("Creating concrete Ethereum = " + coinRepository.save(concreteEthereum));
+            Coin concreteCardano = new Coin(cardano);
+            log.info("Creating concrete Cardano = " + coinRepository.save(concreteCardano));
+            Coin concretePolkadot = new Coin(polkadot);
+            log.info("Creating concrete Polkadot = " + coinRepository.save(concretePolkadot));
 
-            log.info("adding concrete bitcoin to idan's wallet");
-            idan.addCoin(concreteBitcoin);
-            log.info("adding concrete ethereum to idan's wallet");
-            idan.addCoin(concreteEthereum);
-            log.info("idan's stats after adding concrete coins to wallet = \n" + idan);
+            log.info("\n\t\t\tdistributing concrete coins to users . . .");
+            User idan = userRepository.findByIdentityNumber(123L);
+            Wallet w1 = idan.addCoinToWallet(concreteBitcoin);
+            Wallet w2 = idan.addCoinToWallet(concreteEthereum);
+            User daniel = userRepository.findByIdentityNumber(1L);
+            Wallet w3 = daniel.addCoinToWallet(concreteCardano);
+            Wallet w4 = daniel.addCoinToWallet(concretePolkadot);
+            log.info("saving all data to User, Wallet, Coin repositories . . .");
+            coinRepository.save(concreteBitcoin);
+            coinRepository.save(concreteEthereum);
+            coinRepository.save(concreteCardano);
+            coinRepository.save(concretePolkadot);
+            log.info("w1 = " + w1.getId() + " -> " + w1);
+            log.info("w2 = " + w2.getId() + " -> " + w2);
+            log.info("w3 = " + w3.getId() + " -> " + w3);
+            log.info("w4 = " + w4.getId() + " -> " + w4);
+            walletRepository.save(w1);
+            walletRepository.save(w2);
+            walletRepository.save(w3);
+            walletRepository.save(w4);
+            log.info("idan = " + userRepository.save(idan));
+            log.info("daniel = " + userRepository.save(daniel));
         };
     }
 }
