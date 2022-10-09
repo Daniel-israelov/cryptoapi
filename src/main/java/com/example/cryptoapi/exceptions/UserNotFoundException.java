21bfdf0c4d7257693c85a1c038e013939d5b8510
Daniel Israelov <svhrkz@gmail.com>
2022-10-09 14:42:10 +0300

    added exception cases

--- src/main/java/com/example/cryptoapi/exceptions/UserNotFoundException.java
+++ src/main/java/com/example/cryptoapi/exceptions/UserNotFoundException.java
@@ -2,7 +2,17 @@ package com.example.cryptoapi.exceptions;
 
 public class UserNotFoundException extends RuntimeException {
 
+    private static final String baseMessage = "There is no User corresponding to ";
+
     public UserNotFoundException(Long identityNumber) {
-        super("There is no User corresponding to identityNumber = " + identityNumber);
+        super(baseMessage + "identityNumber = " + identityNumber);
+    }
+
+    public UserNotFoundException(String firstName, String lastName) {
+        super(baseMessage + "name = " + firstName + " " + lastName);
+    }
+
+    public UserNotFoundException(String gender) {
+        super("Invalid input: '" + gender + "'");
     }
 }
